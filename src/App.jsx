cc33c9961e9a1e048d6936f37e3966683290a194
valyy151 <marin.valenta1@gmail.com>
2023-04-12 13:28:07 +0200

    Basic Routes and Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
 import './App.css'
+import { HomePage } from './pages/HomePage'
+import { LoginPage } from './pages/LoginPage'
+import { AboutPage } from './pages/AboutPage'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { NavBar } from './components/NavBar'
 
 function App() {
-
-  return (
-    <div className="App">
-      <h1 className='text-center text-5xl text-sky-400 mt-20'>Hello World</h1>
-    </div>
-  )
+	return (
+		<div className='App bg-gray-100 dark:bg-gray-900'>
+			<BrowserRouter>
+				<NavBar></NavBar>
+				<Routes>
+					<Route
+						path='/'
+						element={<HomePage />}
+					/>
+					<Route
+						path='/about'
+						element={<AboutPage />}
+					/>
+					<Route
+						path='/login'
+						element={<LoginPage />}
+					/>
+				</Routes>
+			</BrowserRouter>
+		</div>
+	)
 }
 
 export default App
