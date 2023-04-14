a0a3b6e89333e33341aa7d3aa7ba580a8bcbc5b4
valyy151 <marin.valenta1@gmail.com>
2023-04-14 14:33:37 +0200

    merge

--- src/pages/ArticleDetailsPage.jsx
+++ src/pages/ArticleDetailsPage.jsx
@@ -6,34 +6,32 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 
 function ArticleDetailsPage() {
-    const [article, setArticle] = useState([])
+	const [article, setArticle] = useState([])
 
+	const { articleId } = useParams()
 
-    const { articleId } = useParams()
+	console.log(articleId)
 
-    console.log(articleId)
+	useEffect(() => {
+		getArticle()
+	}, [])
 
-    useEffect(() => {
-        getArticle()
-    }, [])
+	const getArticle = () => {
+		axios
+			.get(`http://localhost:5005/api/community/article/${articleId}`)
+			.then(({ data }) => {
+				setArticle(data)
+			})
+			.catch((e) => {
+				console.log('fail to access database..', e)
+			})
+	}
 
-    const getArticle = () => {
-        axios
-            .get(`http://localhost:5005/api/community/article/${articleId}`)
-            .then(({ data }) => {
-                setArticle(data)
-            })
-            .catch((e) => {
-                console.log('fail to access database..', e)
-            })
-    }
-
-
-    return (
-        <div className='container mx-auto p-4 space-y-8'>
-            <ArticleDetails article={article} />
-        </div>
-    )
+	return (
+		<div className='container mx-auto p-4 space-y-8'>
+			<ArticleDetails article={article} />
+		</div>
+	)
 }
 
 export default ArticleDetailsPage
