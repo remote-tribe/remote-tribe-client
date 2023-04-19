9f7741bd7467e751a7c86a6f24219a452b2b8379
valyy151 <marin.valenta1@gmail.com>
2023-04-19 12:40:09 +0200

    Homepage styling

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -31,9 +31,44 @@ export const HomePage = () => {
 					/>
 				</div>
 			) : (
-				<h1 className='text-gray-800 dark:text-gray-200 text-5xl mt-20 text-center'>
-					Hello {loggedUser ? loggedUser.username : 'World'}
-				</h1>
+				<main className='h-full flex items-center px-6 lg:px-32 bg-sky-500 dark:bg-sky-900 text-white'>
+					<section className='text-gray-700 body-font'>
+						<div className='container mx-auto flex px-5 py-20 md:flex-row flex-col items-center'>
+							<div className='lg:max-w-3xl lg:w-full  md:w-1/2 w-5/6 mb-10 md:mb-0'>
+								<img
+									className='object-cover object-center rounded-md shadow-md'
+									alt='hero'
+									src='https://images.unsplash.com/photo-1593642634315-48f5414c3ad9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80'
+								/>
+							</div>
+							<div className='lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center'>
+								<h1
+									style={{ fontFamily: 'MuseoModerno' }}
+									className='title-font font-moderno sm:text-6xl text-3xl mb-4 font-medium text-sky-800 dark:text-sky-400 '>
+									Remote Tribe{' '}
+								</h1>
+								<h2 className='title-font sm:text-4xl text-2xl mb-4 font-medium text-white '>
+									Work Remotely as a Developer and Live on Your Own Terms
+								</h2>
+
+								<p className='mb-8 leading-relaxed font-medium text-white '>
+									Finding a remote developer job can be daunting - but it doesn't have to be. At Remote Tribe,
+									we've made it easy. Our platform connects you with the best remote job opportunities from
+									around the world, and our community of like-minded developers provides support and guidance
+									every step of the way. Join the tribe today and start your remote work journey.
+								</p>
+								<div className='flex justify-center'>
+									<button className='inline-flex text-white bg-sky-600 border-0 py-2 px-6 focus:outline-none hover:bg-sky-700 rounded text-xl transition-all duration-150 shadow'>
+										Explore
+									</button>
+									<button className='ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-300 hover:text-gray-900 rounded text-xl transition-all duration-150 shadow'>
+										Join Now
+									</button>
+								</div>
+							</div>
+						</div>
+					</section>
+				</main>
 			)}
 		</div>
 	)
